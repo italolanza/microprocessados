d7d4db64c88f5d34656b5f359557b9ac37cc6e95
joao-poloni <joao.poloni@ibm.com>
2018-10-18 18:33:18 -0300

    Lab2/ex1.asm done

--- microprocessados/lab2/ex1.asm
+++ microprocessados/lab2/ex1.asm
@@ -27,14 +27,14 @@ movc a,@a+dptr
 mov dptr,#dados_display	;envia o dados para display para dptr 
 movx @dptr,a			;envia 3fh para display
 
-loop:
-jnb sw, loop
-jb sw, $
-inc r0
+loop: ; Rotina para verificar se o botao sw foi precssionada
+jnb sw, loop ; sw foi pressionada?, sim: proxima linha. nao: pula para loop
+jb sw, $ ; sw continua pressionada?, sim: mantem preso na linha. nao: proxima linha
+inc r0 ; incrementa r0
 
-cjne r0, #00010000b, inicio
-mov r0,#0
-ljmp inicio
+cjne r0, #00010000b, inicio ; compara se r0 e 10H, sim: proxima linha. nao: pula para inicio
+mov r0,#0 ; move o valor 0 para r0
+ljmp inicio ; pula para o inicio
 tabela:
 	; 0	  1	  2	  3   4   5   6   7   8   9   A   B   C   D   E   F
 db	 3fh,06h,5bh,4fh,66h,6dh,7dh,07h,7fh,67h,77h,7ch,39h,5eh,79h,71h	
