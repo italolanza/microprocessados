f9ae4b0b41f905acfbe7cc5caaee6f5617a42316
joao-poloni <joao.poloni@ibm.com>
2018-10-18 18:34:55 -0300

    Projeto 1 WIP

--- projetos/Proj01/proj2.asm
+++ projetos/Proj01/proj2.asm
@@ -1,6 +1,9 @@
+TEMP EQU (65535 - 50000)
 TECLADO equ 0FFE3H
 MOTOR equ 0FFE6H
 TECLAAUX equ 11H
+EN_DIPSLAY equ 0ffe1h 
+DADOS_DISPLAY equ 0ffe0h
 
 ORG 0h
 
@@ -28,12 +31,8 @@ TECLADO_READ:
     MOV P1,#02H
     LJMP TECLADO_SAIR
 
-    TR3: CJNE A,#80H,TR4 ; Pressionou tecla "3"?
+    TR3: CJNE A,#80H,TR5 ; Pressionou tecla "3"?
     MOV P1,#03H
-    LJMP TECLADO_SAIR
-
-    TR4: CJNE A,#04H,TR5 ; Pressionou tecla "A"?
-    MOV P1,#0AH
     LJMP TECLADO_SAIR
 
     TR5: CJNE A,#10H,TR6 ; Pressionou tecla "4"?
@@ -44,13 +43,13 @@ TECLADO_READ:
     MOV P1,#05H
     LJMP TECLADO_SAIR
 
-    TR7: CJNE A,#08H,TR8 ; Pressionou tecla "6"?
+    TR7: CJNE A,#08H,TR9_A ; Pressionou tecla "6"?
     MOV P1,#06H
     LJMP TECLADO_SAIR
 
-    TR8: CJNE A,#02H,TR9_A ; Pressionou tecla "B"?
-    MOV P1,#0BH
-    LJMP TECLADO_SAIR
+    ;TR8: CJNE A,#02H,TR9_A ; Pressionou tecla "B"?
+    ;MOV P1,#0BH
+    ;LJMP TECLADO_SAIR
 
     TR9_A: CJNE A,#06H,TR9_B
     MOV A,#TECLAAUX
@@ -68,32 +67,23 @@ TECLADO_READ:
     MOV P1,#08H
     LJMP TECLADO_SAIR
 
-    TR11: CJNE A,#08H, TR12 ; Pressionou tecla "9"?
+    TR11: CJNE A,#08H, TR14 ; Pressionou tecla "9"?
     MOV P1,#09H
     LJMP TECLADO_SAIR
 
-    TR12: CJNE A,#02H, TR13 ; Pressionou tecla "C"?
-    MOV P1,#0CH
-    LJMP TECLADO_SAIR
-
-    TR13: CJNE A,#40H, TR14 ; Pressionou tecla "E"?
-    MOV P1,#0EH
-    LJMP TECLADO_SAIR
-
-    TR14: CJNE A,#20H, TR15 ; Pressionou tecla "0"?
+    TR14: CJNE A,#20H, TR17 ; Pressionou tecla "0"?
+    INC R1
     MOV P1,#00H
     LJMP TECLADO_SAIR
 
-    TR15: CJNE A,#80H, TR16 ; Pressionou tecla "F"?
-    MOV P1,#0FH
-    LJMP TECLADO_SAIR
-
-    TR16: CJNE A,#04H, TR17 ; Pressionou tecla "D"?
-    MOV P1,#0DH
-
     TR17: LJMP TECLADO_SAIR
+    CJNE R1, #00000000, TECLADO_READ
+    ; aqui devo fazer o display apareceer o numero
+    
+    LJMP TECLADO_READ
 
-    ;Esta rotina evita repetição de teclas TECLADO_SAIR:
+    ;Esta rotina evita repetição de teclas
+TECLADO_SAIR:
     PUSH ACC
     MOV A,#80H
     MOV DPTR,#TECLADO MOVX @DPTR,A
@@ -109,5 +99,23 @@ TECLADO_READ:
     MOVX A,@DPTR
     CJNE A,#00H,TECLADO_REPEAT_2 POP ACC
     RET
+
+TEMPO:		;rotina de tempo(20x50.000)
+    MOV R0,#20
+
+    LOOP:
+    MOV TMOD,#00000001B
+    MOV TL0,#LOW(TEMP)
+    MOV TH0,#HIGH(TEMP)
+    SETB TR0
+    JNB TF0,$
+    CLR TR0
+    CLR TF0
+    DJNZ R0,LOOP
+    RET
+
+tabela:
+	; 0	  1	  2	  3   4   5   6   7   8   9   A   B   C   D   E   F
+db	 3fh,06h,5bh,4fh,66h,6dh,7dh,07h,7fh,67h,77h,7ch,39h,5eh,79h,71h
 
 END
